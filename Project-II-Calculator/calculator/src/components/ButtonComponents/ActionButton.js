4cc9ac02002cf8d07a768940a1b914e1eb45172c
AJLambda <apj2277@gmail.com>
2019-03-19 13:52:17 -0700

    finished all js components

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+//Create an <ActionButton /> component that will be used for the zero character and the clear button.
+//My button should also be able to accept dynamic props actionButton for styling
+//My component should be able to render a dynamic prop called text
+
+
+const ActionButton = (props) => {
+    return (
+        <button className={props.actionButton}>{props.text}</button>
+    );
+};
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Display.css';
+
+//Create a <CalculatorDisplay /> component that will be used as the calculator display
+//My display should also be able to accept dynamic props displayStyle for styling
+//My component should be able to render a dynamic prop called text
+
+
+const CalculatorDisplay = (props) => {
+    return (
+        <div className={props.displayStyle}>{props.text}</div>
+    );
+};
+
+export default CalculatorDisplay;
