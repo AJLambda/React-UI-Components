7db9d2a6345f6638411a8c2a66ad719c4e58633b
AJLambda <apj2277@gmail.com>
2019-03-19 13:35:46 -0700

    created number button

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+//Create a <NumberButton /> component that can accept props and display any number/symbol passed down as text.
+//Example your component should be able to render a dynamic prop called text:
+//Your button button should also be able to accept dynamic props buttonStyle for styling
+
+const NumberButton = (props) => {
+    return (
+        <button className={props.buttonStyle}>{props.text}</button>
+    );
+};
+
+export default NumberButton;
